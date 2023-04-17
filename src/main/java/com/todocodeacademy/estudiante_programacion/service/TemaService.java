219ddb1a1c3fef6dc63e51d4d2f050b3c1e1dd44
marcosjpaez <marcos@marcospaez.com.ar>
2023-04-17 11:15:52 -0300

    primer commit

---
+++ src/main/java/com/todocodeacademy/estudiante_programacion/service/TemaService.java
@@ -0,0 +1,39 @@
+package com.todocodeacademy.estudiante_programacion.service;
+
+import com.todocodeacademy.estudiante_programacion.model.Tema;
+import com.todocodeacademy.estudiante_programacion.repository.ITemaRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TemaService implements ITemaService{
+
+    @Autowired
+    private ITemaRepository temaRepo;
+    
+    @Override
+    public List<Tema> getTemas() {
+        return temaRepo.findAll();
+    }
+
+    @Override
+    public void saveTema(Tema tema) {
+        temaRepo.save(tema);    }
+
+    @Override
+    public void deleteTema(Long id) {
+        temaRepo.deleteById(id);
+    }
+
+    @Override
+    public Tema findTema(Long id) {
+        return temaRepo.findById(id).orElse(null);
+    }
+
+    @Override
+    public void editTema(Tema tema) {
+        this.saveTema(tema);
+    }
+    
+}
